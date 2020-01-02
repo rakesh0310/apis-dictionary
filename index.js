d642b0dbec2710e6977453d906bba89513e71731
rakesh0310 <rakesh03101999@gmail.com>
2020-01-02 13:04:52 +0530

    Added routes for each specifics

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 8001;
 
- 
 const wordsRoutes = require('./api/routes/words');
 const wordRoutes = require('./api/routes/word');
+const userRoutes = require('./api/routes/user');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use('/words', wordsRoutes);
 app.use('/word', wordRoutes);
+app.use('/user',userRoutes)
+
+app.use((req, res, next) =>{
+	const error = new Error("route Not found");
+	error.status = 404;
+	next(error);
+})
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error:{
+			message: error.message
+		}
+	})
+})
 
 app.listen( PORT, function () {
 	console.log(`server running on the ${PORT}`);
