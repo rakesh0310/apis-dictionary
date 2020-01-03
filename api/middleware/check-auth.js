d9832e67f6c150c1ae2d75b733700352810c59e9
rakesh0310 <rakesh03101999@gmail.com>
2020-01-03 13:33:35 +0530

    Modified to get environmental variables

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 module.exports = (req,res, next) => {
     try{
-        const decoded = jwt.verify(req.body.token, 'secret');
+        const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
         req.userData = decoded;
         next();
     } catch (err) {
