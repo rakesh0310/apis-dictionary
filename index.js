b0d657fcbb7a2071165f5eb51759252351e24a69
rakesh0310 <47220814+rakesh0310@users.noreply.github.com>
2020-01-13 18:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ const flash = require('connect-flash');
 const app = express();
 const port = process.env.PORT;
 
-<<<<<<< HEAD
 // passport config
 require('./api/config/passport')(passport);
 
-mongoose.connect('mongodb+srv://rest-api1:'+process.env.MONGO_ATLAS_PW+'@rest-apis1-mqraa.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://rest-api1:rest123@rest-apis1-mqraa.mongodb.net/test?retryWrites=true&w=majority', {
         useNewUrlParser: true,useUnifiedTopology: true
     }).then( () => console.log("mongo connected")).catch(err => console.log(err));
 
@@ -41,50 +40,6 @@ app.use(session({
 // passport
 app.use(passport.initialize());
 app.use(passport.session());
-=======
-const getRoute = (req) => {
-    const route = req.route ? req.route.path : ''; // check if the handler exist
-    const baseUrl = req.baseUrl ? req.baseUrl : ''; // adding the base url if the handler is child of other handler
-
-    return route ? `${baseUrl === '/' ? '' : baseUrl}${route}` : 'unknown route';
-}
-
-const fs = require('fs');
-const FILE_PATH = 'stats.json';
-
-// read json object from file
-const readStats = () => {
-    let result = {};
-    try {
-        result = JSON.parse(fs.readFileSync(FILE_PATH));
-    } catch (err) {
-        console.error(err);
-    }
-    return result;
-}
-
-// dump json object to file
-const dumpStats = (stats) => {
-    try {
-        fs.writeFileSync(FILE_PATH, JSON.stringify(stats), { flag: 'w+' });
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-app.use((req, res, next) => {
-    res.on('finish', () => {
-        const stats = readStats();
-        if(res.statusCode === 200){
-            const event = `${getRoute(req)} ${res.statusCode}`;
-            stats[event] = stats[event] ? stats[event] + 1 : 1;
-        }
-        
-        dumpStats(stats);
-    })
-    next()
-})
->>>>>>> eb8aea1c61788127c5dea8e493ebfa03a1992c0d
 
 
 // connect flash
@@ -118,7 +73,6 @@ app.use( (req, res, next) => {
 app.use('/',homeRoutes);
 app.use('/words', wordsRoutes);
 app.use('/word', wordRoutes);
-<<<<<<< HEAD
 app.use('/user', userRoutes);
 app.get('/stats',  async (req, res, next) => {
 	const stats =await statRoutes();
@@ -126,12 +80,6 @@ app.get('/stats',  async (req, res, next) => {
 });
 
 
-=======
-app.use('/user',userRoutes);
-app.get('/stats/', (req, res) => {
-    res.json(readStats())
-});
->>>>>>> eb8aea1c61788127c5dea8e493ebfa03a1992c0d
 app.use((req, res, next) =>{
 	const error = new Error("route Not found");
 	error.status = 404;
