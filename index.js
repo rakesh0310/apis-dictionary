5a83f53fc6c14718e34346afa3307ee400e76e2b
rakesh0310 <rakesh03101999@gmail.com>
2020-01-13 12:20:41 +0530

    Updated to use connect-flash,sessions and created new routes to stats.js

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose') 
+const passport = require('passport');
+const mongoose = require('mongoose');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+
 
 const app = express();
-const PORT = 8001;
+const port = process.env.PORT;
+
+// passport config
+require('./api/config/passport')(passport);
+
+mongoose.connect('mongodb+srv://rest-api1:'+process.env.MONGO_ATLAS_PW+'@rest-apis1-mqraa.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,useUnifiedTopology: true
+    }).then( () => console.log("mongo connected")).catch(err => console.log(err));
 
 
+
+app.set('views', path.join(__dirname, './api/views'));
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs'); 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+// Express sessiom
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// connect flash
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+	res.locals.error = req.flash('error');
+    next();
+})
 
 const wordsRoutes = require('./api/routes/words');
 const wordRoutes = require('./api/routes/word');
 const userRoutes = require('./api/routes/user');
+const homeRoutes = require('./api/routes/home');
+const statRoutes = require('./api/routes/stats');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+
 
 app.use( (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -23,25 +69,32 @@ app.use( (req, res, next) => {
 	next();
 });
 
+
+app.use('/',homeRoutes);
 app.use('/words', wordsRoutes);
 app.use('/word', wordRoutes);
-app.use('/user',userRoutes)
+app.use('/user', userRoutes);
+app.get('/stats',  async (req, res, next) => {
+	const stats =await statRoutes();
+	res.json(stats);
+});
+
 
 app.use((req, res, next) =>{
 	const error = new Error("route Not found");
 	error.status = 404;
 	next(error);
-})
+});
 
 app.use((error, req, res, next) => {
-	res.status(error.status || 500);
-	res.json({
+	res.status(error.status || 500).json({
 		error:{
 			message: error.message
 		}
 	})
-})
+});
 
-app.listen( PORT, function () {
-	console.log(`server running on the ${PORT}`);
+
+app.listen( port, function () {
+	console.log(`server running on the ${port}`);
 });
